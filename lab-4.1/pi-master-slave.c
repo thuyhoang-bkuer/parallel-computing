a4260da78ed31dbf6ae9d79b3a0da6ede9707d8a
Trong Thuy <thuy.hoang_bkuer17@hcmut.edu.vn>
2019-10-16 12:53:18 +0000

    Before merge macbook

--- lab-4.1/pi-master-slave.c
+++ lab-4.1/pi-master-slave.c
@@ -7,10 +7,11 @@
 #define MASTER 0
 
 
-int masterWork(int argc, char** argv, int nprocs, int DEBUG = 0) {
+int masterWork(int argc, char** argv, int nprocs) {
+    int DEBUG = 0;
     int nslaves;
     long points, extra;
-    long total, inside = 0;
+    long total, s_inside = 0, r_inside;
 
     if (argc != 2) {
         printf("There's must be 2 args. e.g. `mpirun -np 25 ./pims 100000000`");
@@ -23,8 +24,12 @@ int masterWork(int argc, char** argv, int nprocs, int DEBUG = 0) {
         MPI_Abort(MPI_COMM_WORLD, -1);
         exit(0);
     }
-
     nslaves = nprocs - 1;
+    
+    printf("Pi will be computed by %d slaves.\n", nslaves);
+    
+    double start = MPI_Wtime();
+    
     total = atoi(argv[1]);
     
     points = total / nslaves;
@@ -33,20 +38,24 @@ int masterWork(int argc, char** argv, int nprocs, int DEBUG = 0) {
     MPI_Bcast(&points, 1, MPI_LONG, MASTER, MPI_COMM_WORLD);
     MPI_Bcast(&extra, 1, MPI_LONG, MASTER, MPI_COMM_WORLD);
 
-    if (DEBUG) printf("Broadcast successfully!\n");
+    printf("Broadcast successfully!\n");
 
     printf("Waiting for slaves..\n");
 
-    MPI_Reduce(&inside, &inside, 1, MPI_LONG, MPI_SUM, MASTER, MPI_COMM_WORLD);
+    MPI_Reduce(&s_inside, &r_inside, 1, MPI_LONG, MPI_SUM, MASTER, MPI_COMM_WORLD);
+    
+    double finish = MPI_Wtime();
 
-    double pi = ((double) inside) / total;
+    printf("Job done in %.7lf.\n", finish - start);
+    double pi = (4.0 * r_inside) / total;
     printf("pi = %.7lf\n", pi);
 }
 
 
-int slaveWork(int rank, int DEBUG = 0) {
+int slaveWork(int rank) {
+    int DEBUG = 0;
     long points, extra;
-    long inside;
+    long s_inside = 0, r_inside;
 
     if (rank <= extra) points += 1;
 
@@ -61,10 +70,10 @@ int slaveWork(int rank, int DEBUG = 0) {
         x = (float) rand() / RAND_MAX;
         y = (float) rand() / RAND_MAX;
 
-        if (x*x + y*y < 1) inside++;
+        if (x*x + y*y < 1) s_inside++;
     }
 
-    MPI_Reduce(&inside, &inside, 1, MPI_LONG, MPI_SUM, MASTER, MPI_COMM_WORLD);
+    MPI_Reduce(&s_inside, &r_inside, 1, MPI_LONG, MPI_SUM, MASTER, MPI_COMM_WORLD);
 }
 
 
